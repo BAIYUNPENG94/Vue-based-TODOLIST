a72d364f10c7ee7d336bef04639e34b71e564bc6
Yunpeng.Bai <baiyunpeng@yahoo.com>
2022-06-18 03:16:13 +0900

    a
    a

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,15 @@ import VueRouter from 'vue-router'
 import Dashboard from '../views/Dashboard.vue'
 import Projects from '../views/Projects.vue'
 import Team from '../views/Team.vue'
+import Test from '../views/Test.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Dashboard',
-    component: Dashboard 
+    name: 'Test',
+    component: Test 
   },
   {
     path: '/projects',
@@ -21,6 +22,11 @@ Vue.use(VueRouter)
     path: '/team',
     name: 'Team',
     component: Team
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
   },
 ]
 
